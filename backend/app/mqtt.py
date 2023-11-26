d8923eaa5eb52c4a1ef33f85dae3524d87587fed
Xavier Codinas <xavier19966@gmail.com>
2023-11-26 01:18:52 +0100

    Always return all data in mqtt but when is not passed return -1

--- backend/app/mqtt.py
+++ backend/app/mqtt.py
@@ -7,5 +7,14 @@ client.username_pw_set("grupo_2", "grupo_2")
 client.connect("84.88.76.18", 1883, 60)
 
 
+def format_data(data):
+    return json.dumps({
+        "dhtmode": data.get("dhtmode") or -1,
+        "pump": data.get("pump") or -1,
+        "day": data.get("day") if data.get('day') is not None else -1,
+        "fruit": data.get("fruit") or -1,
+    })
+
+
 def publish_data(data):
-    client.publish("hackeps/G2", json.dumps(data))
+    client.publish("hackeps/G2", format_data(data))
